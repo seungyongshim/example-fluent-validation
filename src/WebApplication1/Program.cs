39de8350102b77b5c7582aff123ab24aae3da303
SeungYongShim <syshim@me.com>
2022-07-16 00:21:13 +0900

    .

--- src/WebApplication1/Program.cs
+++ src/WebApplication1/Program.cs
@@ -1,7 +1,10 @@
+using System.Reflection;
 using FluentValidation;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Dto;
+using LanguageExt;
+using static LanguageExt.Prelude;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -13,6 +16,8 @@ builder.Services.Configure<ApiBehaviorOptions>(options =>
     options.SuppressModelStateInvalidFilter = true;
 });
 
+builder.Services.AddValidatorsFromAssemblies(new[] { Assembly.GetExecutingAssembly() });
+
 var app = builder.Build();
 
 ValidatorOptions.Global.LanguageManager.Enabled = false;
@@ -23,10 +28,18 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.MapPost( "/NotificationMail", (NotificationMail dto) =>
+app.MapPost( "/NotificationMail", (NotificationMail dto, IValidator<NotificationMail> validator) =>
 {
-    return dto;
-}).WithName("GetWeatherForecast");
+
+    var ret = validator.Validate(dto);
+
+    if (ret.IsValid is not true)
+    {
+        return Results.ValidationProblem(ret.ToDictionary());
+    }
+
+    return Results.Ok(dto);
+});
 
 
 await app.RunAsync();
