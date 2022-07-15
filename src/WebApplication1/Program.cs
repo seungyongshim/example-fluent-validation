7fc76594ed1f578dcc9694508d034ea2563f025a
SeungYongShim <syshim@me.com>
2022-07-16 08:31:11 +0900

    .

--- src/WebApplication1/Program.cs
+++ src/WebApplication1/Program.cs
@@ -4,7 +4,10 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Mvc;
 using WebApplication1.Dto;
 using LanguageExt;
+using LanguageExt.Common;
 using static LanguageExt.Prelude;
+using System.Text.Json;
+using System.Linq;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -22,24 +25,39 @@ var app = builder.Build();
 
 ValidatorOptions.Global.LanguageManager.Enabled = false;
 
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.MapPost( "/NotificationMail", (NotificationMail dto, IValidator<NotificationMail> validator) =>
+app.MapPost("/NotificationMail", async (HttpRequest dto, IValidator<NotificationMail> validator) =>
 {
+    Console.WriteLine(dto.ContentLength);
 
-    var ret = validator.Validate(dto);
 
-    if (ret.IsValid is not true)
+
+    var q = from _ in unitEff
+            from req in JsonSerializer.DeserializeAsync<NotificationMail>(dto.BodyReader.AsStream(), new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            }).ToAff()
+            from valid in validator.ValidateAndThrowAsync(req).ToUnit().ToAff()
+            select req;
+
+    var ret = await q.Run();
+
+    return ret.Match(Results.Ok, err => err.Exception.Case switch
     {
-        return Results.ValidationProblem(ret.ToDictionary());
-    }
-
-    return Results.Ok(dto);
-});
+        ValidationException e => Results.ValidationProblem(e.Errors.GroupBy(x => x.PropertyName)
+                                                                   .ToDictionary(
+                                                                        g => g.Key,
+                                                                        g => g.Select(x => x.ErrorMessage).ToArray()
+                                                                    )),
+        _ => Results.Problem(err.ToString())
+    });
+}).Accepts<NotificationMail>("application/json");
 
 
 await app.RunAsync();
